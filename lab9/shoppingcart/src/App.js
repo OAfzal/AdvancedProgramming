c54d67cc24b4b710f665f81fea4040e80fac72a4
Osama <osama.afzal97@gmail.com>
2019-11-14 21:38:02 +0500

    Fresh Start

--- lab9/shoppingcart/src/App.js
+++ lab9/shoppingcart/src/App.js
@@ -1,17 +1,6 @@
 import React, {Component} from 'react';
-import Login from './Login';
-import Register from './Register';
-
 
 class App extends Component {
-    render(){
-        return(
-            <div>
-                <Login />
-                <Register />
-            </div>
-        );
-    }
 
 }
 
